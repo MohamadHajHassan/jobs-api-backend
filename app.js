ea7dfe760262549b46d704524938a090b9e401c4
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-04 21:43:21 +0200

    setup express server

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("jobs api");
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Jobs API");
+});
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port} `);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
