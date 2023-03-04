69497df28fd7f6ad3e88763ca0bec375fe714c94
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-04 22:17:05 +0200

    add not found middleware

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const notFoundMiddleware = require("./middlewares/not-found");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Jobs API");
 });
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 3000;
 const start = async () => {
