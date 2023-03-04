c3088cfc70eb2da7efe73e5d85f0200d2bb57c3d
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-04 23:17:04 +0200

    connect database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("express-async-errors");
 
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
 
 const notFoundMiddleware = require("./middlewares/not-found");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
@@ -19,6 +20,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, () => {
             console.log(`Server is listening on port ${port} `);
         });
