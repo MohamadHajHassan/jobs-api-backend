431e7b5626735db1e6363cc416c414916e2a0c01
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-06 00:14:28 +0200

    add create job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,16 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+
+const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userID;
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({ job });
+};
 const getAllJobs = (req, res) => {
     res.send("get all jobs!");
 };
 const getJob = (req, res) => {
     res.send("get single job!");
-};
-const createJob = (req, res) => {
-    res.send("create job!");
 };
 const updateJob = (req, res) => {
     res.send("update job!");
