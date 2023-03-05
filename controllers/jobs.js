1757a881d22d5b0a86b7fb8f978141a3250d6233
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-06 00:47:38 +0200

    add get single job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,20 +1,38 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
+const NotFoundError = require("../errors/not-found");
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userID;
     const job = await Job.create(req.body);
     res.status(StatusCodes.CREATED).json({ job });
 };
+
 const getAllJobs = async (req, res) => {
     const jobs = await Job.find({ createdBy: req.user.userID }).sort(
         "createdAt"
     );
     res.status(StatusCodes.OK).json({ count: jobs.length, jobs });
 };
-const getJob = (req, res) => {
-    res.send("get single job!");
+
+const getJob = async (req, res) => {
+    const {
+        user: { userID },
+        params: { id: jobID },
+    } = req;
+
+    const job = await Job.findOne({
+        createdBy: userID,
+        _id: jobID,
+    });
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobID}`);
+    }
+
+    res.status(StatusCodes.OK).json({ job });
 };
+
 const updateJob = (req, res) => {
     res.send("update job!");
 };
