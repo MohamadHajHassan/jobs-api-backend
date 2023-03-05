5bf60bed2a3f6ffaa61dbc6a1a7f08797501cf27
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-05 20:05:17 +0200

    add login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const UnauthorizedError = require("../errors/unauthorized");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
@@ -7,8 +8,21 @@ const register = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
-const login = (req, res) => {
-    res.send("user logged in!");
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new UnauthorizedError("Invalid Credentials");
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+        throw new UnauthorizedError("Invalid Credentials");
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
