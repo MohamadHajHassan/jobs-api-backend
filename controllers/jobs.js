4c0c839d9e3a6c940b22a464666592fa8d093fe5
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-03-06 21:12:24 +0200

    add update job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,7 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const NotFoundError = require("../errors/not-found");
+const BadRequestError = require("../errors/bad-request");
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userID;
@@ -33,9 +34,33 @@ const getJob = async (req, res) => {
     res.status(StatusCodes.OK).json({ job });
 };
 
-const updateJob = (req, res) => {
-    res.send("update job!");
+const updateJob = async (req, res) => {
+    const {
+        body: { company, position },
+        user: { userID },
+        params: { id: jobID },
+    } = req;
+
+    if (company === "" || position === "") {
+        throw new BadRequestError("Please provide both company and position");
+    }
+
+    const job = await Job.findByIdAndUpdate(
+        {
+            createdBy: userID,
+            _id: jobID,
+        },
+        req.body,
+        { new: true, runValidators: true }
+    );
+    
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobID}`);
+    }
+
+    res.status(StatusCodes.OK).json({ job });
 };
+
 const deleteJob = (req, res) => {
     res.send("delete job!");
 };
